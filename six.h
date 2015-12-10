12535924442c3d2084e0e515d630af65999fc897
Rob <robertipk@gmail.com>
2015-12-09 23:29:54 -0500

    Delete debugging print statements. Reformatted the printDirections function. Delete unnecessary intAttempts-- line.

--- six.h
+++ six.h
@@ -10,43 +10,44 @@ using namespace std;
 
 class Six
 {
-public:
-    Six()
-    {
-        cout << "Get Ready to test your skills "<<endl<<endl;
-        printDirections();
-        score = 0;
-        numFailed = 0;
-        numRemaining = 10;
-        loadQuestions(questionArray);
-        for (int i=0;i<10;i++)
-        {
-            cout << "Question Number " << i+1 << endl;
-            queryUser(questionArray[i]);
-        }
-    }
-    struct Question
-    {
-       string question;
-       string stringAnswer; //string, to be converted to cstring
-       char answer[100]; //cstring
-       int attemptsLeft; //starts with three attempts
-       string sixLetterWord;
-       bool solved; //flag changed to true if user answers correctly
-       Question() //constructor
-       {
-        attemptsLeft = 3;
-        solved = false;
-       }
-    };
-    void queryUser(Question);
-    void printDirections();
-    void loadQuestions(Question []);
+  public:
+      Six()
+      {
+          cout << "Get ready to test your skills! "<<endl<<endl;
+          printDirections();
+          score = 0;
+          numFailed = 0;
+          numRemaining = 10;
+          loadQuestions(questionArray);
+          for (int i=0;i<10;i++)
+          {
+              cout << "Question Number " << i+1 << endl;
+              queryUser(questionArray[i]);
+          }
+          cout << "Game over! Thank you for playing. Your final score is: " << score << "!" << endl;
+      }
+            struct Question
+            {
+               string question;
+               string stringAnswer; //string, to be converted to cstring
+               char answer[100]; //cstring
+               int attemptsLeft; //starts with three attempts
+               string sixLetterWord;
+               bool solved; //flag changed to true if user answers correctly
+               Question() //constructor
+               {
+                attemptsLeft = 3;
+                solved = false;
+               }
+            };
+            void queryUser(Question);
+            void printDirections();
+            void loadQuestions(Question []);
 
-private:
-    int score; //number of questions solved successfully
-    int numFailed; //number questions failed
-    int numRemaining; //number of questions left to solve
-    Question questionArray [20]; 
-};
+  private:
+            int score; //number of questions solved successfully
+            int numFailed; //number questions failed
+            int numRemaining; //number of questions left to solve
+            Question questionArray [20]; 
+  };
 #endif
