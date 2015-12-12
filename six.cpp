b78377d2389695a55727690052b41cd2c4cea66f
Rob <robertipk@gmail.com>
2015-12-12 12:44:54 -0500

    User asked to type y/n before each question is asked

--- six.cpp
+++ six.cpp
@@ -7,59 +7,68 @@ void Six::queryUser(Question q)
     {
         char skip[5] = "skip";
         char giveup[8] = "give up";
+        char yes[2] = "y";
         char userAnswer[100];
         char lowerCase[100];
+        char * test; //pointer to first occurrence of userAnswer in q.answer
+        char reply[2];
         int length;
-        cout << q.question << endl;
-        cout << "The six-letter word is: " << q.sixLetterWord << endl;
-        cout << "Please enter your answer: " << endl;
-        cin.clear(); 
-        cin.getline(userAnswer, 100);
-        cout << "You typed: " << userAnswer << endl;
+        cout << "Would you like to solve a clue? y/n" << endl;
+        cin.clear();
+        cin.getline(reply, 2);
+        if (strcmp(reply,yes) == 0){
 
-        while (q.solved == false && q.attemptsLeft >0)
-        {
-            for(int i = 0; i<25; i++){
-                userAnswer[i] = tolower(userAnswer[i]);                 
-                }
-            if (strcmp(userAnswer,q.answer)==0)
+            cout << q.question << endl;
+            cout << "The six-letter word is: " << q.sixLetterWord << endl;
+            cout << "Please enter your answer: " << endl;
+            cin.clear(); 
+            cin.getline(userAnswer, 100);
+            cout << "You typed: " << userAnswer << endl;
 
+            while (q.solved == false && q.attemptsLeft >0)
             {
-                cin.clear();
-                score++;
-                q.solved = true;
-                cout << "Correct! Your current score has been increased to: " << score << endl;
-            }
-            else if (strcmp(userAnswer,skip) == 0)
-            {
-                cout << "You chose to skip this question. Moving on to next question..." << endl;
-                break;
-            }
-            else if (strcmp(userAnswer,giveup) == 0)
-            {
-                cout << "You chose to give up. The answer is " << q.answer << ". Moving on to next question. "<< endl;
-                break;
-            }
-            else
-            {
-                q.attemptsLeft--;
-                cout << "Incorrect! You have " << q.attemptsLeft << " attempts remaining!" << endl;
-                if (q.attemptsLeft>0){
-                    cout << q.question << endl;  
-                    cout << "The six-letter word is: " << q.sixLetterWord << endl;
+                for(int i = 0; i<25; i++){
+                    userAnswer[i] = tolower(userAnswer[i]);
+                    test = strstr(userAnswer,q.answer); //if user's answer is found WITHIN the correct answer                
+                    }
+                if (strcmp(userAnswer,q.answer)==0 || test != 0)
+                {
                     cin.clear();
-                    cin.getline(userAnswer, 100);
-                    cout << endl;
+                    score++;
+                    q.solved = true;
+                    cout << "Correct! Your current score has been increased to: " << score << endl;
                 }
-		
+                else if (strcmp(userAnswer,skip) == 0)
+                {
+                    cout << "You chose to skip this question. Moving on to next question..." << endl;
+                    break;
+                }
+                else if (strcmp(userAnswer,giveup) == 0)
+                {
+                    cout << "You chose to give up. The answer is " << q.answer << ". Moving on to next question. "<< endl;
+                    break;
+                }
+                else
+                {
+                    q.attemptsLeft--;
+                    cout << "Incorrect! You have " << q.attemptsLeft << " attempts remaining!" << endl;
+                    if (q.attemptsLeft>0){
+                        cout << q.question << endl;  
+                        cout << "The six-letter word is: " << q.sixLetterWord << endl;
+                        cin.clear();
+                        cin.getline(userAnswer, 100);
+                        cout << endl;
+                    }
+    		
+                }
             }
-        }
 
-        if (q.attemptsLeft == 0)
-        {  
-            cout << "You have failed your three attempts at this question :(" << endl;
-            cout << "Moving on to the next question..." << endl;
-        }
+            if (q.attemptsLeft == 0)
+            {  
+                cout << "You have failed your three attempts at this question :(" << endl;
+                cout << "Moving on to the next question..." << endl;
+            }
+        }    
     }
 
 void Six::printDirections()
@@ -74,7 +83,10 @@ void Six::printDirections()
         cout << "Followed by the six letter word PERSON."<<endl;
         cout << "The numbers indicate that the first word of the answer has 7 letters and the second word has 6 letters."<<endl;
         cout << "The word person results from adding the first 3 letters of each word: \"Per\" for the first and \"son\"for the second."<<endl;
-        cout <<"The answer would be perfect sonnet."<<endl<<endl<<"Now Let the games begin :D"<<endl<<endl;  
+        cout << "The answer would be perfect sonnet."<<endl<<endl<<"Now Let the games begin :D"<<endl<<endl;
+        cout << "Enter 'skip' to skip a question. Enter 'give up' to see the answer." << endl;
+        cout << "You will score one point for each correct zero points."<< endl;
+        cout << "No points will be awarded for skipping or giving up on a question." << endl; 
     
 
     }
